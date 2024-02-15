2e91c673ba3b90eddf9285c5077469b13c962d1b
Nic Barr <code@nicholasbarr.net>
2024-02-14 22:08:25 -0600

    initial from fork

---
+++ src/Controllers/JsonController.php
@@ -0,0 +1,14 @@
+<?php
+    namespace Bearlovescode\LaravelTraits\Controllers;
+
+    use Illuminate\Http\JsonResponse;
+
+    trait JsonController
+    {
+        public function respondWithJson(mixed $data,
+                                        int $status = 200,
+                                        array $headers = []): JsonResponse
+        {
+            return response()->json($data, $status, $headers);
+        }
+    }
---
+++ src/Models/UlidModel.php
@@ -0,0 +1,20 @@
+<?php
+    namespace Bearlovescode\LaravelTraits\Models;
+
+    use Illuminate\Database\Eloquent\Concerns\HasUlids;
+    use Illuminate\Database\Eloquent\SoftDeletes;
+
+    trait UlidModel
+    {
+        use HasUlids, SoftDeletes;
+
+        public function getKeyType(): string
+        {
+            return 'ulid';
+        }
+
+        public function getIncrementing(): bool
+        {
+            return false;
+        }
+    }
